7d9c467a8a0d2e2fa4f85486eac633285c04fdcf
unknown <wiman1235@gmail.com>
2024-10-04 10:14:13 +0700

    update program

--- mqtt/mqtt.js
+++ mqtt/mqtt.js
@@ -1,10 +1,10 @@
 const host = "s77d0011.ala.dedicated.gcp.emqxcloud.com";
 const port = "8083";
 
-const urlMQTT = `ws://${host}:${port}/ws`;
+const urlMQTT = `ws://${host}:${port}/mqtt`;
 
 const mqttConfig = {
-  username: "/sidikdik12:sidikdik12",
+  username: "sidikdik12",
   password: "wiman123",
   clientId: `wiman_${new Date().getTime().toString()}`,
   protocolId: "MQTT",
